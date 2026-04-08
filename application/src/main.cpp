0e6b7c1a0942f34b4e6d109da05eb91ec1499e94
LoganBrownGU <2641407B@student.gla.ac.uk>
2026-04-08 14:27:54 +0100

    Added example flex data

--- application/src/main.cpp
+++ application/src/main.cpp
@@ -1,3 +1,8 @@
+<<<<<<< Updated upstream
+=======
+#include <array>
+#include <chrono>
+>>>>>>> Stashed changes
 #include <cmath>
 #include <cstdint>
 #include <cstdio>
@@ -28,6 +33,31 @@ int main(int argc, char **argv) {
     (void) argc;
     (void) argv; 
 
+<<<<<<< Updated upstream
+=======
+    ButtonDriver bd;
+    TLC59711 leds(17, 27);
+    FlexSensor flexSensor;
+    // TLC59711::Channels channels;
+    
+    /* 
+    flexSensor.registerCallback([&channels, &leds] (std::array<ExtensionData, 4> data) {
+        channels[0] = data[0];
+        leds.update(channels);
+    });
+    */
+    
+    auto start = std::chrono::high_resolution_clock::now();
+    flexSensor.registerCallback([start] (std::array<ExtensionData, 4> data) {
+        std::cout << std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - start).count() << ",";
+        for (int i = 0; i < 4; i++)
+            printf("%.10f,", data[i]);
+        std::cout << std::endl;
+    });   
+    
+    flexSensor.begin();
+    getchar();
+>>>>>>> Stashed changes
     
     return 0;
 }
