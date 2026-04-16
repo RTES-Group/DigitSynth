8651790b479a8065fa07e86a7508c38b689a47cd
Finn McConville <finnmcco03@gmail.com>
2026-04-16 16:36:20 +0100

    implement synth logic level button handling for rewritten rules

--- application/src/SynthController.cpp
+++ application/src/SynthController.cpp
@@ -1,9 +1,34 @@
 #include "SynthController.hpp"
+#include <iostream>
 
 SynthController::SynthController(TLC59711& tlc)
-: ledController(tlc);
+: ledController(tlc)
 {
     this->buttonDriver.registerSingleButtonCallback([this] (int index) {
+        if (modeManager.getCurrentMode() == NORMAL){
+            switch(index){
+                case 0: // enter chord mode
+                    modeManager.updateMode();
+                    break;
+                case 1: // enable/disable the LFO
+                    lfoManager.toggle();
+                    break;
+                case 2: // cycle through the LFO shapes
+                    lfoManager.cycleShape();
+                    break;
+                case 3: // turn off/on
+                    
+                    break;
+            }
+        }
+        else { // chord mode
+            if (index == chordManager.getCurrentChord()){
+                modeManager.updateMode(); // exit chord mode, back to normal
+            }
+            else {
+                chordManager.updateChord(index);
+            }
+        }
     });
     
     this->flexSensor.registerCallback([this] (std::array<ExtensionData, 4> values){
