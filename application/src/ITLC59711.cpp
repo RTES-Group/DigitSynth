26ca2d62b6c27157f403bac7ef63ff8b0826b58c
Finn McConville <finnmcco03@gmail.com>
2026-04-17 12:49:32 +0100

    add ITLC9711 interface for dependency injection

---
+++ application/src/ITLC59711.cpp
@@ -0,0 +1,8 @@
+//
+//  ITLC59711.cpp
+//  
+//
+//  Created by Finn McConville on 17/04/2026.
+//
+
+#include "ITLC59711.hpp"
---
+++ application/src/ITLC59711.hpp
@@ -0,0 +1,13 @@
+//
+//  ITLC59711.hpp
+//  
+//
+//  Created by Finn McConville on 17/04/2026.
+//
+
+#ifndef ITLC59711_hpp
+#define ITLC59711_hpp
+
+#include <stdio.h>
+
+#endif /* ITLC59711_hpp */
