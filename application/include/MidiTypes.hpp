ec5c6c64d583a01c1987546b47f9cc1ec672d996
Finn McConville <finnmcco03@gmail.com>
2026-04-20 20:26:03 +0100

    Doxygen comments for MidiTypes header

--- application/include/MidiTypes.hpp
+++ application/include/MidiTypes.hpp
@@ -4,22 +4,34 @@
 
 #include <cstdint>
 
+/**
+  * Designates the control mode for the synth logic layer.
+ */
 enum ControlMode {
     NORMAL,
     CHORD
 };
 
+/**
+  * Designates the LED pattern for use by LedController.
+ */
 enum LedPattern {
     STATUS,
     RIPPLE
 };
 
+/**
+  * Designates the LFO shape; used by LedController and the MIDI output layer.
+ */
 enum LfoShape {
     SIN,
     SQR,
     SH
 };
 
+/**
+  * Stores an abstract MIDI message
+ */
 struct midi_message {
     uint8_t status;
     uint8_t data_1;
