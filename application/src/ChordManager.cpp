d1b4eab3de00b20621c3a72385ebced29ca10fc5
jwedlinscky <wedlinj01@gmail.com>
2026-04-20 18:51:13 +0100

    removed erronious doxygen from cpp

--- application/src/ChordManager.cpp
+++ application/src/ChordManager.cpp
@@ -1,25 +1,14 @@
 #include "ChordManager.hpp"
 
-/**
- * updateChord changes the current chord index to the given index, which is used to determine which notes to output
- * 
- */
+
 void ChordManager::updateChord(int index){
     currentChordIndex = index;
 }
 
-/**
- * getCurrentChord returns the current chord index
- * 
- */
 uint8_t ChordManager::getCurrentChord(){
     return currentChordIndex;
 }
 
-/**
- * getNote returns the note at the given index for the current chord
- * 
- */
 uint8_t ChordManager::getNote(int i){
     // i loops 0-5 for the number of notes in the chord
     // this function gets called 6 times in total for outputting one chord's notes
